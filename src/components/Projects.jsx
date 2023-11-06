09a56ddaf50a23a0db22b909161afb17d0be79c8
Leom <jch.lion97@gmail.com>
2023-11-06 09:36:54 -0500

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,51 +7,32 @@ export const Projects = () => {
   return (
     <section className="flex flex-col items-center h-[calc(100vh-128px)] w-full mb-6 gap-8">
       <h1 className="text-4xl font-fredericka text-orange-600">Projects</h1>
-      <figure className="grid grid-cols-3 grid-rows-3 w-1/2 h-full gap-3 p-8">
-        <div className="group relative bg-white rounded-xl row-span-2 overflow-hidden hover:scale-[103%] transition bg-travelling bg-cover bg-bottom">
-          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-44 group-hover:-bottom-2 group-hover:translate-y-42 transition-all group-hover:from-70%">
-            <div className="flex w-full justify-between">
-              <h1 className="flex flex-col w-full items-start  font-fredericka text-lg text-orange-600 ">
-                Travelling
-              </h1>
-              <div className="flex gap-1">
-                <HtmlIcon className={"w-8"} />
-                <TailwindIcon className={"w-8"} />
-                <JavaScriptIcon className={"w-8"} />
+      <figure className="grid lg:grid-cols-3 grid-cols-1 grid-rows-7 w-1/2 h-full gap-3 p-8">
+        <div className="group relative bg-white rounded-xl row-span-2 overflow-hidden hover:scale-[103%] transition bg-travelling bg-cover bg-bottom ">
+          <a href="https://incomparable-faun-b2c772.netlify.app" target="blank">
+            <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-52 group-hover:-bottom-2 group-hover:translate-y-0 transition-all group-hover:from-70%">
+              <div className="flex w-full justify-between cursor-pointer">
+                <h1 className="flex flex-col w-full items-start  font-fredericka text-lg text-orange-600 ">
+                  Travelling
+                </h1>
+                <div className="flex gap-1">
+                  <HtmlIcon className={"w-8"} />
+                  <TailwindIcon className={"w-8"} />
+                  <JavaScriptIcon className={"w-8"} />
+                </div>
               </div>
-            </div>
-            <div>
-              <p className="font-poppins text-white font-light text-sm pt-4">
-                Some project made by love, using such tools as bla, and bla bla.
-                I found troubles implemeting firebase but I solve them reading
-                carefully the documentation.
-              </p>
-            </div>
-          </section>
+              <div>
+                <p className="font-poppins text-white font-light text-sm pt-4">
+                  Some project made by love, using such tools as bla, and bla
+                  bla. I found troubles implemeting firebase but I solve them
+                  reading carefully the documentation.
+                </p>
+              </div>
+            </section>
+          </a>
         </div>
         <div className="group relative overflow-hidden hover:scale-[103%] transition bg-white rounded-xl">
-          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-44 group-hover:-bottom-2 group-hover:translate-y-42 transition-all group-hover:from-70%">
-            <div className="flex w-full justify-between">
-              <h1 className="flex flex-col w-full items-start  font-fredericka text-lg text-orange-600 ">
-                Project 1
-              </h1>
-              <div className="flex gap-1">
-                <ReactIcon className={"w-8"} />
-                <TailwindIcon className={"w-8"} />
-                <FirebaseIcon className={"w-8"} />
-              </div>
-            </div>
-            <div>
-              <p className="font-poppins text-white font-light text-sm pt-4">
-                Some project made by love, using such tools as bla, and bla bla.
-                I found troubles implemeting firebase but I solve them reading
-                carefully the documentation.
-              </p>
-            </div>
-          </section>
-        </div>
-        <div className="group relative overflow-hidden hover:scale-[103%] transition bg-white rounded-xl">
-          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-44 group-hover:-bottom-2 group-hover:translate-y-42 transition-all group-hover:from-70%">
+          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-52 group-hover:-bottom-2 group-hover:translate-y-0 transition-all group-hover:from-70%">
             <div className="flex w-full justify-between">
               <h1 className="flex flex-col w-full items-start  font-fredericka text-lg text-orange-600 ">
                 Project 1
@@ -72,7 +53,28 @@ export const Projects = () => {
           </section>
         </div>
         <div className="group relative overflow-hidden hover:scale-[103%] transition bg-white rounded-xl">
-          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-44 group-hover:-bottom-2 group-hover:translate-y-42 transition-all group-hover:from-70%">
+          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-52 group-hover:-bottom-2 group-hover:translate-y-0 transition-all group-hover:from-70%">
+            <div className="flex w-full justify-between">
+              <h1 className="flex flex-col w-full items-start  font-fredericka text-lg text-orange-600 ">
+                Project 1
+              </h1>
+              <div className="flex gap-1">
+                <ReactIcon className={"w-8"} />
+                <TailwindIcon className={"w-8"} />
+                <FirebaseIcon className={"w-8"} />
+              </div>
+            </div>
+            <div>
+              <p className="font-poppins text-white font-light text-sm pt-4">
+                Some project made by love, using such tools as bla, and bla bla.
+                I found troubles implemeting firebase but I solve them reading
+                carefully the documentation.
+              </p>
+            </div>
+          </section>
+        </div>
+        <div className="group relative overflow-hidden hover:scale-[103%] transition bg-white rounded-xl">
+          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-52 group-hover:-bottom-2 group-hover:translate-y-0 transition-all group-hover:from-70%">
             <div className="flex w-full justify-between">
               <h1 className="flex flex-col w-full items-start  font-fredericka text-lg text-orange-600 ">
                 Project 1
@@ -93,12 +95,16 @@ export const Projects = () => {
           </section>
         </div>
         <div className="group relative overflow-hidden hover:scale-[103%] transition bg-white rounded-xl bg-gifTool bg-cover ">
-          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-44 group-hover:-bottom-2 group-hover:translate-y-42 transition-all group-hover:from-70%">
-            <div className="flex w-full justify-between">
-              <h1 className="flex flex-col w-full items-start font-fredericka text-lg text-orange-600 ">
+          <a
+            href="https://fabulous-llama-b051a4.netlify.app"
+            target="blank"
+          ></a>
+          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-52 group-hover:-bottom-2 group-hover:translate-y-0 transition-all group-hover:from-70% cursor-pointer">
+            <div className="flex w-full justify-between ">
+              <h1 className="flex flex-col w-full items-start font-fredericka text-lg text-orange-600">
                 Gif Tool
               </h1>
-              <div className="flex gap-1">
+              <div className="flex gap-1 ">
                 <ReactIcon className={"w-8"} />
                 <BootstrapIcon className={"w-8"} />
                 <JavaScriptIcon className={"w-8"} />
@@ -114,7 +120,7 @@ export const Projects = () => {
           </section>
         </div>
         <div className="group relative overflow-hidden hover:scale-[103%] transition bg-white rounded-xl">
-          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-44 group-hover:-bottom-2 group-hover:translate-y-42 transition-all group-hover:from-70%">
+          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-52 group-hover:-bottom-2 group-hover:translate-y-0 transition-all group-hover:from-70%">
             <div className="flex w-full justify-between">
               <h1 className="flex flex-col w-full items-start  font-fredericka text-lg text-orange-600 ">
                 Project 1
@@ -135,7 +141,7 @@ export const Projects = () => {
           </section>
         </div>
         <div className="group relative overflow-hidden hover:scale-[103%] transition bg-white rounded-xl col-span-2">
-          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-44 group-hover:-bottom-2 group-hover:translate-y-42 transition-all group-hover:from-70%">
+          <section className="absolute -bottom-32 mb-2 pt-4 px-2 w-full bg-gradient-to-t from-purple-950 h-52 group-hover:-bottom-2 group-hover:translate-y-0 transition-all group-hover:from-70%">
             <div className="flex w-full justify-between">
               <h1 className="flex flex-col w-full items-start  font-fredericka text-lg text-orange-600 ">
                 Project 1
