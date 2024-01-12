ada5abbc0b5240717269a2d9b17803883776ca92
Leon <jch.lion97@gmail.com>
2024-01-12 11:03:38 -0500

    desing projects updated

--- src/components/DesProjects.jsx
+++ src/components/DesProjects.jsx
@@ -1,5 +1,6 @@
 
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 
 
@@ -12,8 +13,16 @@ export const DesProjects = () => {
       transition={{ duration: 1, type: "spring" }}
       className="flex justify-center items-start w-1/2 h-full pt-16"
     >
-      <h1 className="text-xl font-poppins font-light dark:text-white">
-        Sorry, design projects will be here soon 😳🙇🏼‍♂️.
+      <h1 className="text-xl text-center font-poppins font-light dark:text-white">
+        Sorry, design projects will be here soon 😳🙇🏼‍♂️. <br /> Meantime this{" "}
+        <Link
+          className="text-red-500 hover:text-purple-500"
+          to="https://drive.google.com/file/d/1Wqd6eOnzNJ7XJTlUfp-F7pjtxFTQHlsd/view?usp=sharing"
+          target="blank"
+        >
+          PDF📜
+        </Link>{" "}
+        contains some of my projects.
       </h1>
     </motion.div>
   );
